38e1d76a45cc344f81992e82f7c17fa968c85c69
katesereda1808 <katesereda22@gmail.com>
2022-01-01 22:07:47 +0300

    icons added

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
 const apiKey = '1f0607fa86a77f9c89257262c4b721c6';
-const city = 'Moscow,ru'; // должен меняться
+const city = 'Moscow,ru';
 const forecast=document.querySelector(".forecast")
-// закат и рассвет data.city.sunset
-// data.list[0].weather[0].description - описание погоды
-
 
 fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
 .then(response=>response.json())
 .then(data=>{
-    console.log(data);
-
     document.querySelector(".current__temperature").innerHTML = Math.round(data.list[0].main.temp-273)+'&deg';
-    console.log(data.list[0].main.feels_like);
     document.querySelector(".current__description").innerHTML = data.list[0].weather[0].description;
     document.querySelector(".current__city").innerHTML = data.city.name;
 
-
     for(let i=0; i<6; i++){
-        let date=(data.list[i].dt_txt).slice(10,16);
+        let date=(i==0? 'Сейчас': data.list[i].dt_txt.slice(10,16));
         let icon=data.list[i].weather[0].icon;
         let temp=Math.round(data.list[i].main.temp-273)+'&deg';
-        
         forecast.innerHTML+=`<div class="forecast__item">
         <div class="forecast__time">${date}</div>
-        <div class="forecast__icon">${icon}</div>
+        <div class="forecast__icon icon__${icon}"></div>
         <div class="forecast__temperature">${temp}</div>
         </div>`
     }
-
 
     const renderDetails=(data)=>{
         let pressure=(data.list[0].main.pressure*0.75).toFixed()+' мм';
@@ -43,24 +34,17 @@ fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}
         let container=document.querySelector(`.${className}`).querySelector('.details_item_value');
         container.innerHTML=value;
     }
+    const isDay=(data)=>{
+        let sunrise=data.city.sunrise*1000;
+        let sunset=data.city.sunset*1000;
+        let now=Date.now();
+        return(now>sunrise&&now<sunset);
+    }
+    const renderDayNightTheme=(data)=>{
+        let attrName=isDay(data)?'day':'night';
+        document.documentElement.setAttribute('data-theme', attrName)
+    }
     
-    renderDetails(data)
-
+    renderDetails(data);
+    renderDayNightTheme(data);
 })
-
-
-
-
-
-//функция-прототип, добавляющая 0 в начале (для отображения времени)
-//переводит число в строку
-// Number.prototype.pad = function(size){
-//     var s = String(this);
-//     while (s.length < (size || 2)){s = "0" + s;}
-//     return s;
-// }
-// function getHours(dateTime){
-//     let date = new Date(dateTime);
-//     let hours = date.getHours().pad();
-//     return hours;
-// }
