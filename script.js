a38beffe4041023cdb78bae5def9bb1a819d19fc
katesereda1808 <katesereda22@gmail.com>
2022-09-10 10:41:20 +0300

    loader added

--- script.js
+++ script.js
@@ -86,7 +86,13 @@ getLocation()
     
 
 
-
+let mask = document.querySelector('.mask');
+window.addEventListener('load',()=>{
+    mask.classList.add('hide');
+    setTimeout(() => {
+        mask.remove()
+    }, 600);
+})
 
 
 
